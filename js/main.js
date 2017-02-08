4331c86c53474d02bbde4aada4cf2ce81792db1f
Himanshu Mangla <him.mangla@gmail.com>
2017-02-08 16:59:27 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -189,7 +189,7 @@
 	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
-	var finalDate = '2017/02/28';
+	var finalDate = '2017/03/1';
 
 	$('div#counter').countdown(finalDate)
    	.on('update.countdown', function(event) {
